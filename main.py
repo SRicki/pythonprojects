dd942f3b7b18317fffd5f7626fc8d80904f2c6d2
SRicki <64754594+SRicki@users.noreply.github.com>
2022-01-01 18:13:16 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# A program to detect positive and negative numbers.
+
+x = float(input("Enter any number: "))
+
+if x > 0:
+    print("this is a positive number")
+elif x < 0:
+    print("this is a negative number")
+else:
+    print("the number is zero")
